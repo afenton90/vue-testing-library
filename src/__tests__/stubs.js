6e03a095ab810b96dcc33eff5819f3158ad29e42
Adrià Fontcuberta <afontcu@gmail.com>
2020-04-27 17:48:20 +0200

    Fix stubs test

--- src/__tests__/stubs.js
+++ src/__tests__/stubs.js
@@ -2,8 +2,17 @@ import {render} from '@testing-library/vue'
 import Stubs from './components/Stubs'
 
 test('Form contains search button', () => {
+  const DirectiveMock = {
+    template: '<p>fake template</p>',
+  }
+
   const {getByText} = render(Stubs, {
-    stubs: ['FontAwesomeIcon'],
+    global: {
+      stubs: {
+        directive: DirectiveMock,
+      },
+    },
   })
-  getByText('Search now')
+
+  getByText('fake template')
 })
