779639d566fdd85c1f74397be0b499a86cca568a
Adrià Fontcuberta <afontcu@gmail.com>
2020-04-27 17:29:34 +0200

    Fix inline snapshot

--- src/__tests__/axios-mock.js
+++ src/__tests__/axios-mock.js
@@ -4,11 +4,9 @@ import {render, fireEvent} from '@testing-library/vue'
 import Component from './components/Fetch.vue'
 
 test('mocks an API call when load-greeting is clicked', async () => {
-  axiosMock.get.mockImplementationOnce(() =>
-    Promise.resolve({
-      data: {greeting: 'hello there'},
-    }),
-  )
+  axiosMock.get.mockResolvedValueOnce({
+    data: {greeting: 'hello there'},
+  })
 
   const {html, getByText} = render(Component, {props: {url: '/greeting'}})
 
@@ -16,17 +14,14 @@ test('mocks an API call when load-greeting is clicked', async () => {
 
   expect(axiosMock.get).toHaveBeenCalledTimes(1)
   expect(axiosMock.get).toHaveBeenCalledWith('/greeting')
+
   getByText('hello there')
 
   // You can render component snapshots by using html(). However, bear in mind
   // that Snapshot Testing should not be treated as a replacement for regular
   // tests.
   // More about the topic: https://twitter.com/searls/status/919594505938112512
-  expect(html()).toMatchInlineSnapshot(`
-    <div><button>
-        Fetch
-      </button> <span>
-        hello there
-      </span></div>
-  `)
+  expect(html()).toMatchInlineSnapshot(
+    `<div><button> Fetch </button><span>hello there</span></div>`,
+  )
 })
